16f025ef684283045145be58bd1c1b2c53db99dd
Dilyara <252658@astanait.edu.kz>
2025-12-20 20:23:11 +0500

    A version of Hospital Management System

---
+++ src/com/dilyara/hospital/Main.java
@@ -0,0 +1,12 @@
+package com.dilyara.hospital;
+
+public class Main {  public static void main(String[] args) {
+
+    System.out.println("Welcome to the Hospital Management System!");
+    System.out.println("This project is developed as part of an OOP course.");
+    System.out.println("The system will manage patients, doctors, and appointments.");
+    System.out.println("Project domain: Hospital");
+    System.out.println("Author: Dilyara Bauyrzhan");
+
+}
+}
